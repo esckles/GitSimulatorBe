0225291154c3e5d81b8f725f2a531987be3a6958
esckles <davidsamzzywilliams1234@gmail.com>
2025-05-07 13:34:34 +0100

    fe

--- dist/utils/email.js
+++ dist/utils/email.js
@@ -1,18 +1,45 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ForgetAccountPasswordEmail = exports.CreateAccountPasswordEmail = void 0;
+exports.ForgetAccountPasswordEmail = exports.CreateAccountPasswordEmail =
+  void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 const googleapis_1 = require("googleapis");
 dotenv_1.default.config();
@@ -24,7 +51,11 @@ const GOOGLE_ID = process.env.GOOGLE_ID;
 const GOOGLE_SECRET = process.env.GOOGLE_SECRET;
 const GOOGLE_URL = process.env.GOOGLE_URL;
 const GOOGLE_TOKEN = process.env.GOOGLE_TOKEN;
-const oAuth = new googleapis_1.google.auth.OAuth2(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_URL);
+const oAuth = new googleapis_1.google.auth.OAuth2(
+  GOOGLE_ID,
+  GOOGLE_SECRET,
+  GOOGLE_URL
+);
 oAuth.setCredentials({ refresh_token: GOOGLE_TOKEN });
 // export const CreateAccountPasswordEmail = async (user: any) => {
 //   const accessToken: any = (await oAuth.getAccessToken()).token;
@@ -60,18 +91,19 @@ oAuth.setCredentials({ refresh_token: GOOGLE_TOKEN });
 //     console.log("Email Send📧");
 //   });
 // };
-const CreateAccountPasswordEmail = (user) => __awaiter(void 0, void 0, void 0, function* () {
+const CreateAccountPasswordEmail = (user) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const accessToken = (yield oAuth.getAccessToken()).token;
     const Transporter = nodemailer_1.default.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.GOOGLE_MAIL,
-            type: "OAuth2",
-            clientId: GOOGLE_ID,
-            clientSecret: GOOGLE_SECRET,
-            refreshToken: GOOGLE_TOKEN,
-            accessToken: accessToken,
-        },
+      service: "gmail",
+      auth: {
+        user: process.env.GOOGLE_MAIL,
+        type: "OAuth2",
+        clientId: GOOGLE_ID,
+        clientSecret: GOOGLE_SECRET,
+        refreshToken: GOOGLE_TOKEN,
+        accessToken: accessToken,
+      },
     });
     // const token = jwt.sign(
     //   {
@@ -82,7 +114,7 @@ const CreateAccountPasswordEmail = (user) => __awaiter(void 0, void 0, void 0, f
     // );
     // const URL_value = `https://gitsimulator-fe.web.app/auth/login/${token}`;
     const id = user === null || user === void 0 ? void 0 : user._id;
-    const URL_value = `https://gitsimulator-fe.web.app/auth/login/${id}`;
+    const URL_value = `https://gitsimulator-fe.web.app/auth/otp/${id}`;
     // const URL_value = `https://gitsimulator-fe.web.app/auth/login/${token}`;
     // const FRONTEND_URL =
     //   process.env.NODE_ENV === "development"
@@ -90,43 +122,52 @@ const CreateAccountPasswordEmail = (user) => __awaiter(void 0, void 0, void 0, f
     //     : process.env.FRONTEND_PROD_URL;
     // console.log("ENV:", process.env.NODE_ENV);
     // const URL_value = `${FRONTEND_URL}/auth/login/${token}`;
-    const pathFile = node_path_1.default.join(__dirname, "../views/createaccount.ejs");
+    const pathFile = node_path_1.default.join(
+      __dirname,
+      "../views/createaccount.ejs"
+    );
     const html = yield ejs_1.default.renderFile(pathFile, {
-        name: user === null || user === void 0 ? void 0 : user.name,
-        otp: user === null || user === void 0 ? void 0 : user.otp,
-        time: user === null || user === void 0 ? void 0 : user.otpExpiresAT,
-        url: URL_value,
+      name: user === null || user === void 0 ? void 0 : user.name,
+      otp: user === null || user === void 0 ? void 0 : user.otp,
+      time: user === null || user === void 0 ? void 0 : user.otpExpiresAT,
+      url: URL_value,
     });
     const mailer = {
-        to: user === null || user === void 0 ? void 0 : user.email,
-        from: `<${process.env.GOOGLE_MAIL}>`,
-        subject: "Account creation",
-        text: "just text message",
-        html,
+      to: user === null || user === void 0 ? void 0 : user.email,
+      from: `<${process.env.GOOGLE_MAIL}>`,
+      subject: "Account creation",
+      text: "just text message",
+      html,
     };
     yield Transporter.sendMail(mailer).then(() => {
-        console.clear();
-        console.log("Email Sent📧");
+      console.clear();
+      console.log("Email Sent📧");
     });
-});
+  });
 exports.CreateAccountPasswordEmail = CreateAccountPasswordEmail;
-const ForgetAccountPasswordEmail = (user) => __awaiter(void 0, void 0, void 0, function* () {
+const ForgetAccountPasswordEmail = (user) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const accessToken = (yield oAuth.getAccessToken()).token;
     const Transporter = nodemailer_1.default.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.GOOGLE_MAIL,
-            type: "OAuth2",
-            clientId: GOOGLE_ID,
-            clientSecret: GOOGLE_SECRET,
-            refreshToken: GOOGLE_TOKEN,
-            accessToken: accessToken,
-        },
+      service: "gmail",
+      auth: {
+        user: process.env.GOOGLE_MAIL,
+        type: "OAuth2",
+        clientId: GOOGLE_ID,
+        clientSecret: GOOGLE_SECRET,
+        refreshToken: GOOGLE_TOKEN,
+        accessToken: accessToken,
+      },
     });
-    const token = jsonwebtoken_1.default.sign({
+    const token = jsonwebtoken_1.default.sign(
+      {
         id: user === null || user === void 0 ? void 0 : user._id,
-    }, process.env.JWT_SECRET, { expiresIn: (process.env.JWT_EXIRES, 10) });
-    const FRONTEND_URL = process.env.NODE_ENV === "development"
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: (process.env.JWT_EXIRES, 10) }
+    );
+    const FRONTEND_URL =
+      process.env.NODE_ENV === "development"
         ? process.env.FRONTEND_DEV_URL
         : process.env.FRONTEND_PROD_URL;
     const URL_value = `${FRONTEND_URL}/auth/change-ps/${token}`;
@@ -136,23 +177,26 @@ const ForgetAccountPasswordEmail = (user) => __awaiter(void 0, void 0, void 0, f
     //     : `https://gitsimulator-fe.web.app/auth/change-ps/${token}`;
     // const URL_value = "https://gitsimulator-fe.web.app";
     // const URL_value = `https://gitsimulator-fe.web.app/auth/change-ps/${token}`;
-    const pathFile = node_path_1.default.join(__dirname, "../views/forgetaccount.ejs");
+    const pathFile = node_path_1.default.join(
+      __dirname,
+      "../views/forgetaccount.ejs"
+    );
     const html = yield ejs_1.default.renderFile(pathFile, {
-        name: user === null || user === void 0 ? void 0 : user.name,
-        // otp: user?.otp,
-        // time: user?.otpExpiresAT,
-        url: URL_value,
+      name: user === null || user === void 0 ? void 0 : user.name,
+      // otp: user?.otp,
+      // time: user?.otpExpiresAT,
+      url: URL_value,
     });
     const mailer = {
-        to: user === null || user === void 0 ? void 0 : user.email,
-        from: `<${process.env.GOOGLE_MAIL}>`,
-        subject: "Reset Password ",
-        text: "just text message",
-        html,
+      to: user === null || user === void 0 ? void 0 : user.email,
+      from: `<${process.env.GOOGLE_MAIL}>`,
+      subject: "Reset Password ",
+      text: "just text message",
+      html,
     };
     yield Transporter.sendMail(mailer).then(() => {
-        console.clear();
-        console.log("Email Sent📧");
+      console.clear();
+      console.log("Email Sent📧");
     });
-});
+  });
 exports.ForgetAccountPasswordEmail = ForgetAccountPasswordEmail;
